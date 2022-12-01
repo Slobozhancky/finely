828e8a931e9a408b5d316f72853c511b494c7e8d
Slobozhancky <46904434+Slobozhancky@users.noreply.github.com>
2022-12-01 22:54:21 +0200

    feat: add header

--- src/assets/modules/tiny-slider.js
+++
@@ -1,8 +0,0 @@
-import { tns } from "tiny-slider";
-
-var slider = tns({
-    container: ".my-slider",
-    items: 1,
-    slideBy: "page",
-    autoplay: true,
-});
--- src/index.js
+++ src/index.js
@@ -1,14 +1,2 @@
-import "./styles/style.css";
-import "./styles/scss.scss";
+import "./styles/main.scss";
 
-
-const body = document.querySelector('.box');
-const h1 = document.createElement('button');
-
-console.log(body);
-
-h1.innerHTML = 'Hello world';
-
-console.log(h1);
-
-body.appendChild(h1);
